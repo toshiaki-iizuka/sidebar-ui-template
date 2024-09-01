8c5722ee1d4b011d3cef243004a1f9f2c293c1aa
Toshiaki Iizuka <t_112ka@yahoo.co.jp>
2024-09-01 16:11:58 +0900

    style: add breadcrumb

--- app/(contents)/dashboard/page.tsx
+++ app/(contents)/dashboard/page.tsx
@@ -1,12 +1,34 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import { Card, CardContent } from "@/components/ui/card";
 import Content from "@/components/layouts/Content";
 
 const Dashboard = () => {
   return (
     <Content title="Dashboard">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Dashboard</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
       <Card className="rounded-lg border-none mt-6">
         <CardContent className="p-6">
           <div className="flex justify-center items-center min-h-[calc(100vh-56px-64px-20px-24px-56px-48px)]">
