4606634f16febf3f178a727b638a7a7b51e3dbd8
Toshiaki Iizuka <t_112ka@yahoo.co.jp>
2024-09-01 12:34:00 +0900

    style: update root page to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,7 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
-import { ModeToggle } from "@/components/theme/ModeToggle";
-import { PanelsTopLeft } from "lucide-react";
+import Dashboard from "@/app/dashboard/page";
 
-export default function Home() {
-	return (
-		<div className="flex flex-col min-h-screen">
-			<header className="z-[50] sticky top-0 w-full bg-background/95 border-b backdrop-blur-sm dark:bg-black/[0.6] border-border/40">
-				<div className="container h-14 flex items-center">
-					<Link
-						href="/"
-						className="flex justify-start items-center hover:opacity-85 transition-opacity duration-300"
-					>
-						<PanelsTopLeft className="w-6 h-6 mr-3" />
-						<span className="font-bold">sidebar-ui-template</span>
-						<span className="sr-only">sidebar-ui-template</span>
-					</Link>
-					<nav className="ml-auto flex items-center gap-2">
-						<Button
-							variant="outline"
-							size="icon"
-							className="rounded-full w-8 h-8 bg-background"
-							asChild
-						>
-							<Link
-								href="https://github.com/toshiaki-iizuka/sidebar-ui-template"
-								target="_blank"
-							>
-								<GitHubLogoIcon className="h-[1.2rem] w-[1.2rem]" />
-							</Link>
-						</Button>
-						<ModeToggle />
-					</nav>
-				</div>
-			</header>
-		</div>
-	);
-}
+const Home = () => {
+	return <Dashboard />;
+};
+
+export default Home;
