3b049ff727bc66ba5796e52c3acb528b53a8c07e
Toshiaki Iizuka <t_112ka@yahoo.co.jp>
2024-09-02 11:14:16 +0900

    feat: add sheet menu for sp display

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -1,5 +1,6 @@
-import { ModeToggle } from "@/components/theme/ModeToggle";
 import UserNav from "@/components/layouts/UserNav";
+import SheetMenu from "@/components/layouts/SheetMenu";
+import { ModeToggle } from "@/components/theme/ModeToggle";
 
 interface NavbarProps {
   title: string;
@@ -10,6 +11,7 @@ const Navbar = ({ title }: NavbarProps) => {
     <header className="sticky top-0 z-10 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary">
       <div className="mx-4 sm:mx-8 flex h-14 items-center">
         <div className="flex items-center space-x-4 lg:space-x-0">
+          <SheetMenu />
           <h1 className="font-bold">{title}</h1>
         </div>
         <div className="flex flex-1 items-center space-x-2 justify-end">
