16c7297111dfc568c434c17c819f8dc807da3177
Toshiaki Iizuka <t_112ka@yahoo.co.jp>
2024-09-03 12:05:55 +0900

    fix: wrong path

--- components/layouts/NavigationMenu.tsx
+++ components/layouts/NavigationMenu.tsx
@@ -29,7 +29,7 @@ export const NavigationMenu = () => {
             variant="link"
             asChild
           >
-            <Link href="/dashboard" className="flex items-center gap-2">
+            <Link href="/" className="flex items-center gap-2">
               <PanelsTopLeft className="w-6 h-6 mr-1" />
               <SheetTitle className="font-bold text-lg">
                 Sidebar Template
